02eb9faf0d88db93a2b922f2c789418bb93e0e81
Bashiru200 <bashirubarrie12@gmail.com>
2025-10-05 12:49:46 -0400

    ContextAPI debuging for global state

--- alx-project-0x05/pages/_app.tsx
+++ alx-project-0x05/pages/_app.tsx
@@ -1,11 +1,13 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import Header from "@/components/layouts/Header";
 import { CountProvider } from "@/context/CountContext";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <CountProvider>
-      <Component {...pageProps} />
+      <Header />    {/* Header stays visible on all pages */}
+      <Component {...pageProps} />  {/* This renders the current page */}
     </CountProvider>
-  )
+  );
 }
